1ac4a29dc80c56eb4787516a43f50aa4b7c4f019
Hamzah Contreras <Hamzah_36@outlook.com>
2022-01-12 19:01:27 -0600

    commit tree acd568e40a35c8d486b3831a7f382563d2b6faec

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+// const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// // Code your solution here
 
-/*function callback(accum, element) {
-    return element + accum;
-}*/
+// /*function callback(accum, element) {
+//     return element + accum;
+// }*/
 
-let totalBatteries = batteryBatches.reduce((accum, element) => accum + element, 0);
+// let totalBatteries = batteryBatches.reduce((accum, element) => accum + element, 0);
